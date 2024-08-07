8ff4d5cfee2271d6e921ce99259f31c7670f6c6b
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-08-07 20:28:50 +0200

    Update Header.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
 import logo from "../assets/holberton-logo.jpg";
-import "./Header.css";
+import { StyleSheet, css } from "aphrodite";
+
+const styles = StyleSheet.create({
+  "App-header": {
+    fontSize: "1.4rem",
+    color: "#e0354b",
+    display: "flex",
+    alignItems: "center",
+    padding: "1.2em",
+  },
+
+  img: {
+    width: "250px",
+    height: "250px",
+  },
+});
 
 function Header() {
   return (
     <>
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className={css(styles["App-header"])}>
+        <img src={logo} className={css(styles.img)} alt="logo" />
         <h1>School dashboard</h1>
       </div>
     </>
