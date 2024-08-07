f31ae9e3a706344f8967d20a5f0d009a43d0f89e
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-08-07 20:31:04 +0200

    Update Notifications.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,69 +1,93 @@
-import React, { Component, PureComponent } from "react";
-import "./Notifications.css";
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
 import closeIcon from "../assets/close-icon.png";
 import NotificationItem from "./NotificationItem";
 import PropTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
 
-class Notifications extends PureComponent {
+const styles = StyleSheet.create({
+  Notifications: {
+    padding: "2em",
+    border: "2px dashed red",
+  },
+  menuItem: {
+    textAlign: "right",
+  },
+  "notification-header": {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  "flex-area": {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end",
+  },
+});
+
+class Notifications extends React.Component {
   constructor(props) {
     super(props);
-
     this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.length > this.props.listNotifications.length;
   }
 
   markAsRead(id) {
     console.log(`Notification ${id} has been marked as read`);
   }
 
-  shouldComponentUpdate(nextProps) {
-    return nextProps.listNotifications.length > this.props.listNotifications.length;
-  }
-
   render() {
     return (
       <React.Fragment>
-        <div className="menuItem">
-          <p>Your notifications</p>
-        </div>
         {this.props.displayDrawer ? (
-          <div className="Notifications">
-            <button
-              style={{
-                color: "#3a3a3a",
-                fontWeight: "bold",
-                background: "none",
-                border: "none",
-                fontSize: "15px",
-                position: "absolute",
-                right: "3px",
-                top: "3px",
-                cursor: "pointer",
-                outline: "none",
-              }}
-              aria-label="Close"
-              onClick={(e) => {
-                console.log("Close button has been clicked");
-              }}
-            >
-              <img src={closeIcon} alt="close icon" width="10px" />
-            </button>
-            {this.props.listNotifications.length !== 0 ? <p>Here is the list of notifications</p> : null}
-            <ul>
-              {this.props.listNotifications.length === 0 ? <NotificationItem type="default" value="No new notification for now" /> : null}
-              {this.props.listNotifications.map((val) => (
-                <NotificationItem
-                  type={val.type}
-                  value={val.value}
-                  html={val.html}
-                  key={val.id}
-                  markAsRead={this.markAsRead}
-                  id={val.id}
-                />
-              ))}
-            </ul>
+          <div className={css(styles["flex-area"])}>
+            <div className={css(styles.menuItem)}>
+              <p>Your notifications</p>
+            </div>
+            <div className={css(styles.Notifications)}>
+              <ul>
+                {this.props.listNotifications &&
+                this.props.listNotifications.length > 0 ? (
+                  this.props.listNotifications.map(
+                    ({ id, html, type, value }) => (
+                      <NotificationItem
+                        key={id}
+                        markAsRead={this.markAsRead}
+                        type={type}
+                        value={value}
+                        html={html}
+                      />
+                    )
+                  )
+                ) : (
+                  <div className={css(styles["notification-header"])}>
+                    <NotificationItem value="No new notification for now" />
+                    <button
+                      style={{
+                        border: "none",
+                        background: "none",
+                      }}
+                      aria-label="Close"
+                      onClick={console.log("Close button has been clicked")}
+                    >
+                      <img
+                        style={{ display: "inline" }}
+                        src={closeIcon}
+                        alt="Close"
+                      />
+                    </button>
+                  </div>
+                )}
+              </ul>
+            </div>
           </div>
-        ) : null}
+        ) : (
+          <div className={css(styles.menuItem)}>
+            <p>Your notifications</p>
+          </div>
+        )}
       </React.Fragment>
     );
   }
