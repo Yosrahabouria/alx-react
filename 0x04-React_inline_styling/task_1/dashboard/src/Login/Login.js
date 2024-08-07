6c0a3c4daa9c387e90e1affb934abe41e6289f3f
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2024-08-07 20:29:36 +0200

    Update Login.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,16 +1,39 @@
 import React from "react";
-import "./Login.css";
+import { StyleSheet, css } from "aphrodite";
+
+const styles = StyleSheet.create({
+  "App-body": {
+    fontSize: "1.4rem",
+    padding: "1.2em",
+    height: "45%",
+  },
+
+  "form-inputs": {
+    display: "flex",
+    gap: "2em",
+    alignItems: "center",
+  },
+
+  input: {
+    height: "1.4rem",
+    marginLeft: "10px",
+  },
+});
 
 function Login() {
   return (
     <React.Fragment>
-      <div className="App-body">
+      <div className={css(styles["App-body"])}>
         <p>Login to access the full dashboard</p>
-        <form>
+        <form className={css(styles["form-inputs"])}>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
+          <input className={css(styles.input)} type="email" name="email" />
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
+          <input
+            className={css(styles.input)}
+            type="password"
+            name="password"
+          />
           <button>OK</button>
         </form>
       </div>
