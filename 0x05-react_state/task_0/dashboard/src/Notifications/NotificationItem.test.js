f32e2c79f158d62f41e96ef0a7f95ad2028280e0
Yosrahabouria <yosrahabouria@gmail.com>
2024-08-11 14:37:22 +0200

    work

--- 0x05-react_state/task_0/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,35 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from "react";
 import { shallow } from "enzyme";
-import { getLatestNotification } from "../utils/utils";
-import Notifications from "./Notifications";
 import NotificationItem from "./NotificationItem";
+import { StyleSheetTestUtils } from 'aphrodite';
 
-describe("Notification tests", () => {
-  it("renders Notification component without crashing", () => {
-    const component = shallow(<Notifications />);
+StyleSheetTestUtils.suppressStyleInjection();
 
-    expect(component).toBeDefined();
+describe("<Notifications />", () => {
+  it("NotificationItem renders without crashing", () => {
+    const wrapper = shallow(<NotificationItem />);
+    expect(wrapper.exists()).toEqual(true);
   });
 
-  it("renders correct list items", () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find("ul").children()).toHaveLength(3);
-    expect(wrapper.find("ul").childAt(0).html()).toEqual('<li data-notification-type="default">New course available</li>');
-    expect(wrapper.find("ul").childAt(1).html()).toEqual('<li data-notification-type="urgent">New resume available</li>');
-    expect(wrapper.find("ul").childAt(2).html()).toEqual(`<li data-urgent=\"true\">${getLatestNotification()}</li>`);
+  it("Verify that by passing dummy type and value props, it renders the correct html", () => {
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.find("li")).toHaveLength(1);
+    expect(wrapper.find("li").text()).toEqual("test");
+    expect(wrapper.find("li").prop("data-notification-type")).toEqual("default");
   });
 
-  it("renders an unordered list", () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find("ul").children()).toHaveLength(3);
-    wrapper.find("ul").forEach((node) => {
-      expect(node.equals(<NotificationItem />));
-    });
-  });
-
-  it("renders correct text", () => {
-    const component = shallow(<Notifications />);
-
-    expect(component.find("p").prop("children")).toBe("Here is the list of notifications");
+  it("Passing a html prop, it renders the correct html (for example", () => {
+    const text = "Here is the list of notifications";
+    const wrapper = shallow(<NotificationItem html={{ __html: "<u>test</u>" }} />);
+    expect(wrapper.find("li").html()).toContain('data-notification-type="default"');
   });
 });
--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -2,81 +2,113 @@ import React from "react";
 import PropTypes from "prop-types";
 import { css, StyleSheet } from 'aphrodite';
 import NotificationItem from "./NotificationItem";
-import PropTypes from "prop-types";
+import closeButton from "../assets/close-icon.png";
 import NotificationItemShape from "./NotificationItemShape";
 
-class Notifications extends PureComponent {
-  constructor(props) {
-    super(props);
+const styles = StyleSheet.create({
+  notifications: {
+    border: '3px dashed #e14852',
+    padding: '0.25em',
+    float: 'right',
+    margin: '20px 10px 0 0',
+  },
+  notificationsSmall: {
+    '@media (max-width: 900px)': {
+      float: 'none',
+      fontSize: '20px'
+    }
+  },
+  ulSmall: {
+    '@media (max-width: 900px)': {
+      padding: '0',
+      listStyle: 'none'
+    }
+  },
+  menuItem: {
+    textAlign: 'right',
+    marginRight: '30px'
+  },
+  menuItemSmall: {
+    '@media (max-width: 900px)': {
+      fontSize: '20px'
+    }
+  },
+  notificationsButtonImage: {
+    width: '20px',
+    marginTop: '10px'
+  }
+})
 
-    this.markAsRead = this.markAsRead.bind(this);
+
+class Notifications extends React.Component {
+  constructor (props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this)
   }
 
   markAsRead(id) {
-    console.log(`Notification ${id} has been marked as read`);
+    console.log(`Notification ${id} has been marked as read`)
   }
 
   shouldComponentUpdate(nextProps) {
-    return nextProps.listNotifications.length > this.props.listNotifications.length;
-  }
-
-  render() {
     return (
-      <React.Fragment>
-        <div className="menuItem">
-          <p>Your notifications</p>
-        </div>
-        {this.props.displayDrawer ? (
-          <div className="Notifications">
-            <button
-              style={{
-                color: "#3a3a3a",
-                fontWeight: "bold",
-                background: "none",
-                border: "none",
-                fontSize: "15px",
-                position: "absolute",
-                right: "3px",
-                top: "3px",
-                cursor: "pointer",
-                outline: "none",
-              }}
-              aria-label="Close"
-              onClick={(e) => {
-                console.log("Close button has been clicked");
-              }}
-            >
-              <img src={closeIcon} alt="close icon" width="10px" />
-            </button>
-            {this.props.listNotifications.length !== 0 ? <p>Here is the list of notifications</p> : null}
-            <ul>
-              {this.props.listNotifications.length === 0 ? <NotificationItem type="default" value="No new notification for now" /> : null}
-              {this.props.listNotifications.map((val) => (
-                <NotificationItem
-                  type={val.type}
-                  value={val.value}
-                  html={val.html}
-                  key={val.id}
-                  markAsRead={this.markAsRead}
-                  id={val.id}
-                />
-              ))}
-            </ul>
+      nextProps.listNotifications.length > this.props.listNotifications.length ||
+      nextProps.displayDrawer !== this.props.displayDrawer
+      )
+  }
+  
+  render () { 
+    const { handleDisplayDrawer, handleHideDrawer } = this.props
+    return (
+      <>
+        <div className='menuItem'>
+          <div className={css(styles.menuItem, styles.menuItemSmall)} onClick={handleDisplayDrawer}>
+            Your notifications
           </div>
-        ) : null}
-      </React.Fragment>
+        </div>
+    
+        { this.props.displayDrawer ? 
+          (<div className='Notifications'>
+            <div className={css(styles.notifications, styles.notificationsSmall)}>
+              <button
+                style={{
+                  right: 45,
+                  border: "none",
+                  position: "absolute",
+                  background: "transparent",
+                }}
+                aria-label="close"
+                onClick={handleHideDrawer}
+              >
+              <img src={closeButton} className={css(styles.notificationsButtonImage)} alt="close button icon" />
+              </button>
+              <p>Here is the list of notifications</p>
+              <ul className={css(styles.ulSmall)}>
+                {this.props.listNotifications.length === 0 ? (<NotificationItem value='No new notification for now' type='no-new' />) : <></>}
+                {this.props.listNotifications.map((not) => (<NotificationItem key={not.id} type={not.type} value={not.value} html={not.html} markAsRead={() => {this.markAsRead(not.id)}} />))}
+              </ul>
+            </div>
+          </div>)
+             
+          : <></>
+        }
+      </>
     );
   }
 }
 
-Notifications.propTypes = {
-  displayDrawer: PropTypes.bool,
-  listNotifications: PropTypes.arrayOf(NotificationItemShape),
-};
-
 Notifications.defaultProps = {
   displayDrawer: false,
   listNotifications: [],
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {}
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func,
 };
 
 export default Notifications;
